6124c7018404584e6244ff4d41c24465715e740e
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2023-10-10 11:18:15 -0500

    Adjusted Login Page
    Added Local Storage values
    Cleaned up Imports
    Adjusted Styling
    Added theme toggle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
-	import Logo from '$lib/Logo.svelte';
 	import { browser } from '$app/environment';
-	import { Avatar } from '@skeletonlabs/skeleton';
+	import { enhance } from '$app/forms';
+	import { LightSwitch, localStorageStore } from '@skeletonlabs/skeleton';
+	import type { Writable } from 'svelte/store';
 
 	let desktop: string;
 
@@ -15,52 +15,69 @@
 
 	const agent = window.electron ? 'Electron' : 'Browser';
 
-	let tenant: string = 'tenant';
-	let domain: string = 'identitynow';
+	const tenant: Writable<string> = localStorageStore('tenant', 'tenant');
+	const domain: Writable<string> = localStorageStore('domain', 'identitynow');
+	const baseUrl: Writable<string> = localStorageStore(
+		'baseUrl',
+		'https://${tenant}.api.${domain}.com',
+	);
+	const tenantUrl: Writable<string> = localStorageStore(
+		'tenantUrl',
+		'https://${tenant}.${domain}.com',
+	);
 
-	$: baseUrl = `https://${tenant}.api.${domain}.com`;
-	$: tenantUrl = `https://${tenant}.${domain}.com`;
+	$: baseUrl.set(`https://${$tenant}.api.${$domain}.com`);
+	$: tenantUrl.set(`https://${$tenant}.${$domain}.com`);
 </script>
 
-<main class="p-4">
-	<img src="/SailPoint-Developer-Community-Lockup.png" alt="sailPoint Logo" />
-	<div class="text-2xl text-slate-500 divide-dashed divide-y-2 mt-4 mb-2">
-		Enter your tenant information to continue
+<main class="p-32 relative h-full">
+	<div class="absolute top-2 right-2">
+		<LightSwitch />
 	</div>
-	<form method="POST">
-		<label class="text-slate-600">
-			Tenant
-			<input name="tenant" placeholder={``} bind:value={tenant} class="input p-2" />
-		</label>
-		<label class="text-slate-600">
-			Domain
-			<input name="domain" placeholder={``} bind:value={domain} class="input p-2" />
-		</label>
-		<label class="text-slate-600">
-			API Base URL
-			<input
-				name="baseUrl"
-				placeholder={`https://${tenant}.api.${domain}.com`}
-				bind:value={baseUrl}
-				class="input p-2"
-			/>
-		</label>
+	<div class="flex flex-row justify-center">
+		<img
+			class="h-12 min-w-[590px]"
+			src="/SailPoint-Developer-Community-Lockup.png"
+			alt="sailPoint Logo"
+		/>
+	</div>
+	<div class="">
+		<div class="text-2xl text-center py-2">Enter your tenant information to continue</div>
+		<form method="POST" use:enhance class="flex flex-col gap-4">
+			<label class="">
+				Tenant
+				<input name="tenant" placeholder={``} bind:value={$tenant} class="input p-2" />
+			</label>
+			<label class="">
+				Domain
+				<input name="domain" placeholder={``} bind:value={$domain} class="input p-2" />
+			</label>
+			<label class="">
+				API Base URL
+				<input
+					name="baseUrl"
+					placeholder={`https://${tenant}.api.${domain}.com`}
+					bind:value={$baseUrl}
+					class="input p-2"
+				/>
+			</label>
 
-		<label class="text-slate-600">
-			Tenant URL
-			<input
-				name="tenantUrl"
-				placeholder={`https://${tenant}.identitynow.com`}
-				bind:value={tenantUrl}
-				class="input p-2"
-			/>
-		</label>
+			<label class="">
+				Tenant URL
+				<input
+					name="tenantUrl"
+					placeholder={`https://${tenant}.identitynow.com`}
+					bind:value={$tenantUrl}
+					class="input p-2"
+				/>
+			</label>
 
-		<button type="submit" class="btn variant-filled-primary w-full mt-2 text-slate-50 text-lg">
-			login
-		</button>
-	</form>
+			<button
+				type="submit"
+				class="btn variant-filled-primary w-full mt-2 !text-white text-lg"
+			>
+				Login
+			</button>
+		</form>
+	</div>
 </main>
-
-<style>
-</style>

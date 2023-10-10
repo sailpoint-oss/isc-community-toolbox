8b961f0173466b46947d8b4b65caefa36e0159fa
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2023-10-10 11:17:00 -0500

    Added Navigation sidebar

---
+++ src/lib/sidebar/LeftSidebar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { navigation } from './navigation.ts';
+	import { page } from '$app/stores';
+</script>
+
+<div class="p-2">
+	<div class="flex flex-col gap-2">
+		{#each navigation as section}
+			{section.name}
+			<hr class="!border-t-2" />
+			{#each section.content as link (link.url)}
+				<a
+					class="btn variant-filled-primary !text-white p-2"
+					data-sveltekit-preload-data="hover"
+					href={link.url}
+				>
+					{link.name}
+				</a>
+			{/each}
+		{/each}
+	</div>
+</div>

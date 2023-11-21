66b3277cacf3bcf8fcc8120446925021f980f8d5
Luke Hagar <98849695+luke-hagar-sp@users.noreply.github.com>
2023-11-20 22:54:34 -0600

    Update forge.config.js

--- Electron-App/forge.config.js
+++ Electron-App/forge.config.js
@@ -7,25 +7,20 @@ module.exports = {
   makers: [
     {
       name: "@electron-forge/maker-squirrel",
-      config: {},
     },
     {
       name: "@electron-forge/maker-zip",
-      platforms: ["darwin"],
     },
     {
       name: "@electron-forge/maker-deb",
-      config: {},
     },
     {
       name: "@electron-forge/maker-rpm",
-      config: {},
     },
   ],
   plugins: [
     {
       name: "@electron-forge/plugin-auto-unpack-natives",
-      config: {},
     },
   ],
   publishers: [

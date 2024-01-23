e87d57da81472cf73ddbf9b880c24a307c1316be
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2024-01-22 23:14:09 -0600

    Refactor sidebar component

---
+++ Sveltekit-App/src/lib/sidebar/SidebarDrawer.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Sidebar from './Sidebar.svelte';
+	import { getDrawerStore, Drawer } from '@skeletonlabs/skeleton';
+
+	const drawerStore = getDrawerStore();
+
+	$: classesDrawer = $drawerStore.id === 'doc-sidenav' ? 'lg:hidden' : '';
+	$: console.log($drawerStore.id);
+</script>
+
+<Drawer width="w-[144px]" class={classesDrawer}>
+	{#if $drawerStore.id === 'doc-sidenav'}
+		<!-- Doc Sidebar -->
+		<Sidebar />
+	{/if}
+</Drawer>

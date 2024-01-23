2e5729d2afda31059183ff84cb3ae1a82b9102c9
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2024-01-23 00:00:32 -0600

    Update Progress component layout

--- Sveltekit-App/src/lib/Components/Progress.svelte
+++ Sveltekit-App/src/lib/Components/Progress.svelte
@@ -2,7 +2,7 @@
 	import { ProgressRadial } from '@skeletonlabs/skeleton';
 </script>
 
-<div class="progress-bar">
+<div class="grid place-content-center h-[500px]">
 	<ProgressRadial
 		stroke={100}
 		meter="stroke-primary-500"
@@ -10,10 +10,3 @@
 		class="progress-bar"
 	/>
 </div>
-
-<style>
-	.progress-bar {
-		padding-top: calc(50vh - 4.5rem - 200px);
-		padding-left: calc(50% - 4.5rem);
-	}
-</style>

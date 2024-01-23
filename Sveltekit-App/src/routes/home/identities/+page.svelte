9c99af1c73c37b8e9907aa99398b14ff5a620ff9
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2024-01-22 23:16:48 -0600

    Refactor identities page component

--- Sveltekit-App/src/routes/home/identities/+page.svelte
+++ Sveltekit-App/src/routes/home/identities/+page.svelte
@@ -1,24 +1,18 @@
 <script lang="ts">
-	import { createOnAmountChange, createOnPageChange, createOnGo, formatDate } from '$lib/Utils.js';
+	import Paginator from '$lib/Components/Paginator.svelte';
+	import {
+		createOnAmountChange,
+		createOnPageChange,
+		createOnGo,
+		formatDate,
+		TriggerCodeModal
+	} from '$lib/Utils.js';
 	import type { ModalSettings, PaginationSettings } from '@skeletonlabs/skeleton';
-	import { getModalStore, Paginator } from '@skeletonlabs/skeleton';
+	import { getModalStore } from '@skeletonlabs/skeleton';
 
 	const modalStore = getModalStore();
 
 	export let data;
-
-	function TriggerSourceViewModal(source: any) {
-		const modal: ModalSettings = {
-			type: 'component',
-			component: 'codeBlockModal',
-			meta: {
-				code: JSON.stringify(source, null, 4),
-				language: 'json'
-			}
-		};
-
-		modalStore.trigger(modal);
-	}
 
 	$: onPageChange = createOnPageChange({ ...data.params, filters, sorters }, '/home/identities');
 	$: onAmountChange = createOnAmountChange(
@@ -39,116 +33,74 @@
 </script>
 
 <div class="card flex justify-center flex-col align-middle">
-	<div class=" p-4 flex flex-row justify-between gap-4 flex-wrap">
-		<div class="flex flex-row gap-1">
-			<input
-				on:keydown={onGo}
-				bind:value={filters}
-				class="input"
-				title="Filter"
-				type="text"
-				placeholder="Filter"
-			/>
-			<input
-				on:keydown={onGo}
-				bind:value={sorters}
-				class="input"
-				title="Sorter"
-				type="text"
-				placeholder="Sorter"
-			/>
-			<button on:click={onGo} class="btn variant-filled-primary text-white"> Go </button>
-		</div>
-		<p class="my-auto">Total Count: {data.totalCount}</p>
-		<Paginator
-			bind:settings
-			on:page={onPageChange}
-			on:amount={onAmountChange}
-			showNumerals
-			maxNumerals={1}
-			showFirstLastButtons={true}
-			showPreviousNextButtons={true}
-		/>
+	<Paginator
+		{onAmountChange}
+		{onGo}
+		{onPageChange}
+		{settings}
+		{filters}
+		{sorters}
+		totalCount={data.totalCount}
+	/>
+	<div class="table-container">
+		<table class="table">
+			<thead class="table-head">
+				<th>ID</th>
+				<th>Name</th>
+				<th>Lifecycle State</th>
+				<th>eMail</th>
+				<th>Created</th>
+				<th>Modified</th>
+				<th />
+			</thead>
+			<tbody class="table-body">
+				{#each data.identities as identity}
+					<tr>
+						<td>
+							<p class="text-center">{identity.id}</p>
+						</td>
+						<td>
+							<p class="text-center">{identity.name}</p>
+						</td>
+						<td>
+							<p class="text-center">{identity.lifecycleState?.stateName}</p>
+						</td>
+						<td>
+							<p class="text-center">{identity.emailAddress}</p>
+						</td>
+						<td>
+							<p class="text-center">{formatDate(identity.created)}</p>
+						</td>
+						<td>
+							<p class="text-center">{formatDate(identity.modified)}</p>
+						</td>
+						<td class="flex flex-col justify-center gap-1">
+							<a
+								href={`/home/identities/${identity.id}`}
+								class="btn variant-filled-primary text-sm !text-white"
+								data-sveltekit-preload-data="hover"
+							>
+								Open
+							</a>
+							<button
+								on:click={() => TriggerCodeModal(identity, modalStore)}
+								class="btn variant-filled-primary text-sm !text-white"
+							>
+								View
+							</button>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 	</div>
-	<table class="table">
-		<thead>
-			<th>ID</th>
-			<th>Name</th>
-			<th>Lifecycle State</th>
-			<th>eMail</th>
-			<th>Created</th>
-			<th>Modified</th>
-			<th />
-		</thead>
-		<tbody class="overflow-hidden">
-			{#each data.identities as identity}
-				<tr>
-					<td>
-						<p class="text-center">{identity.id}</p>
-					</td>
-					<td>
-						<p class="text-center">{identity.name}</p>
-					</td>
-					<td>
-						<p class="text-center">{identity.lifecycleState?.stateName}</p>
-					</td>
-					<td>
-						<p class="text-center">{identity.emailAddress}</p>
-					</td>
-					<td>
-						<p class="text-center">{formatDate(identity.created)}</p>
-					</td>
-					<td>
-						<p class="text-center">{formatDate(identity.modified)}</p>
-					</td>
-					<td class="flex flex-col justify-center gap-1">
-						<a
-							href={`/home/identities/${identity.id}`}
-							class="btn variant-filled-primary text-white"
-							data-sveltekit-preload-data="hover"
-						>
-							Open
-						</a>
-						<button
-							on:click={() => TriggerSourceViewModal(identity)}
-							class="btn variant-filled-primary text-white"
-						>
-							View
-						</button>
-					</td>
-				</tr>
-			{/each}
-		</tbody>
-	</table>
-	<div class=" p-4 flex flex-row justify-between gap-4 flex-wrap">
-		<div class="flex flex-row gap-1">
-			<input
-				on:keydown={onGo}
-				bind:value={filters}
-				class="input"
-				title="Filter"
-				type="text"
-				placeholder="Filter"
-			/>
-			<input
-				on:keydown={onGo}
-				bind:value={sorters}
-				class="input"
-				title="Sorter"
-				type="text"
-				placeholder="Sorter"
-			/>
-			<button on:click={onGo} class="btn variant-filled-primary text-white"> Go </button>
-		</div>
-		<p class="my-auto">Total Count: {data.totalCount}</p>
-		<Paginator
-			bind:settings
-			on:page={onPageChange}
-			on:amount={onAmountChange}
-			showNumerals
-			maxNumerals={1}
-			showFirstLastButtons={true}
-			showPreviousNextButtons={true}
-		/>
-	</div>
+	<Paginator
+		{onAmountChange}
+		{onGo}
+		{onPageChange}
+		{settings}
+		{filters}
+		{sorters}
+		totalCount={data.totalCount}
+	/>
 </div>

5dc75a7484a8edb3a8a9198dec8d0184c9268ed9
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2024-01-22 23:16:35 -0600

    Refactor layout component and add sidebar functionality, and breadcrumb navigation

--- Sveltekit-App/src/routes/home/+layout.svelte
+++ Sveltekit-App/src/routes/home/+layout.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import LeftSidebar from '$lib/sidebar/LeftSidebar.svelte';
-	import { AppBar, AppShell, Avatar, LightSwitch } from '@skeletonlabs/skeleton';
+	import { capitalize, parseInitials } from '$lib/Utils';
+
+	import Sidebar from '$lib/sidebar/Sidebar.svelte';
+	import {
+		AppBar,
+		AppShell,
+		Avatar,
+		LightSwitch,
+		getDrawerStore,
+		popup,
+		storePopup,
+		type DrawerSettings,
+		type PopupSettings
+	} from '@skeletonlabs/skeleton';
+
+	import { arrow, autoUpdate, computePosition, flip, offset, shift } from '@floating-ui/dom';
+
+	import HamburgerSvg from '$lib/Components/SVGs/HamburgerSVG.svelte';
+	import SidebarDrawer from '$lib/sidebar/SidebarDrawer.svelte';
+
+	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+
+	export let data;
 
 	let crumbs: Array<{ label: string; href: string }> = [];
 
@@ -9,35 +30,97 @@
 		// Remove zero-length tokens.
 		const tokens = $page.url.pathname.split('/').filter((t) => t !== '');
 
-		// Create { label, href } pairs for each token.
 		let tokenPath = '';
+
 		crumbs = tokens.map((t) => {
 			tokenPath += '/' + t;
-			t = t.charAt(0).toUpperCase() + t.slice(1);
-			return { label: t, href: tokenPath };
-		});
 
-		// Add a way to get home too.
-		// crumbs.unshift({ label: 'Home', href: '/' });
+			return {
+				label: t
+					.split('-')
+					.map((word) => capitalize(word))
+					.join(' '),
+				href: tokenPath
+			};
+		});
 	}
+
+	const drawerStore = getDrawerStore();
+
+	// Drawer Handler
+	function drawerOpen(): void {
+		const s: DrawerSettings = { id: 'doc-sidenav' };
+		drawerStore.open(s);
+	}
+
+	const popupAccount: PopupSettings = {
+		// Represents the type of event that opens/closed the popup
+		event: 'click',
+		// Matches the data-popup value on your popup element
+		target: 'popupAccount',
+		// Defines which side of your trigger the popup will appear
+		placement: 'bottom'
+	};
 </script>
+
+<SidebarDrawer />
 
 <AppShell>
 	<svelte:fragment slot="header">
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<img class="h-8 w-8" src="/logo.ico" alt="SailPoint TetraSail" />
+				<div class="flex items-center space-x-4">
+					<button on:click={drawerOpen} class="btn-icon btn-icon-sm lg:!hidden">
+						<HamburgerSvg class="w-6 h-6" />
+					</button>
+					<img class="h-8 w-8" src="/logo.ico" alt="SailPoint TetraSail" />
+				</div>
 			</svelte:fragment>
-			<p class="text-xl">IdentityNow Admin Console</p>
+			<p class="text-xl lg:!block hidden">IdentityNow Admin Console</p>
 			<svelte:fragment slot="trail">
 				<LightSwitch />
-				<Avatar width="w-10" />
-				<a href="/logout" class="btn variant-filled-primary !text-white text-xs">Logout</a>
+				<div class="rounded-full w-fit" use:popup={popupAccount}>
+					<Avatar
+						initials={parseInitials(data?.userDetails?.user_name)}
+						border="hover:border-2 border-surface-300-600-token hover:!border-primary-500"
+						cursor="cursor-pointer"
+						width="w-10"
+					/>
+					<div class="card p-4 w-72 !shadow-xl bg-surface-100-800-token" data-popup="popupAccount">
+						<div class="arrow bg-surface-50-900-token" />
+						<div class="flex flex-col gap-2">
+							<div class="space-y-4">
+								<Avatar initials={parseInitials(data?.userDetails?.user_name)} width="w-16" />
+								<div>
+									<p class="font-bold">{data?.userDetails?.user_name}</p>
+									<div class="flex flex-wrap gap-4">
+										<small>
+											<span class="opacity-50">Tenant:</span>
+											<strong>{data?.userDetails?.org}</strong>
+										</small>
+										<small>
+											<span class="opacity-50">Pod:</span>
+											<strong>{data?.userDetails?.pod}</strong>
+										</small>
+									</div>
+									<small><span class="opacity-50">Scopes:</span></small>
+									<div class="flex gap-4 flex-wrap">
+										{#each data?.userDetails?.scope as scope}
+											<small><strong></strong>{scope}</small>
+										{/each}
+									</div>
+								</div>
+
+								<a href="/logout" class="btn variant-soft w-full">Logout</a>
+							</div>
+						</div>
+					</div>
+				</div>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">
-		<LeftSidebar />
+		<Sidebar class="hidden lg:grid overflow-hidden" />
 	</svelte:fragment>
 	<!-- <svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment> -->
 	<!-- <svelte:fragment slot="pageHeader">Page Header</svelte:fragment> -->

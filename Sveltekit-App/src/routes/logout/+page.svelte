6bf862c04bb1809c0ce6030ba4e014fc35a8fdd2
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2024-01-23 13:17:34 -0600

    Add invalidateAll() function and update logout page UI

--- Sveltekit-App/src/routes/logout/+page.svelte
+++ Sveltekit-App/src/routes/logout/+page.svelte
@@ -1,24 +1,33 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
+	import { goto, invalidateAll } from '$app/navigation';
 	import { onMount } from 'svelte';
 
 	export let data;
 	onMount(() => {
-		setTimeout(() => {
+		setTimeout(async () => {
 			console.log('Redirecting to login...');
+			await invalidateAll();
 			goto('/');
 		}, 2000);
 	});
 </script>
 
-<div>
-	<h1>IdentityNow Admin Console</h1>
-
-	<br />
-
-	{#if data.sessionLoggedOut}
-		<p>Logged out</p>
-	{:else}
-		<p>Issue logging out</p>
-	{/if}
+<div class="p-4">
+	<div class="card p-4">
+		{#if data.sessionLoggedOut}
+			<p class="text-center p-2">Successfully Logged out</p>
+		{:else}
+			<p class="text-center p-2">
+				WHOOPS! an error occurred. <br /> If you believe this is a bug please submit an issue on
+				<a
+					class="underline text-blue-500 hover:text-blue-700"
+					href="https://github.com/sailpoint-oss/idn-admin-console/issues/new/choose"
+					rel="noreferrer"
+					target="_blank"
+				>
+					GitHub
+				</a>
+			</p>
+		{/if}
+	</div>
 </div>

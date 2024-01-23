c9d69582466f432f0e0d949137ae4e710be33540
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2024-01-23 12:52:11 -0600

    Update Progress component with dynamic width

--- Sveltekit-App/src/lib/Components/Progress.svelte
+++ Sveltekit-App/src/lib/Components/Progress.svelte
@@ -1,9 +1,11 @@
-<script>
+<script lang="ts">
 	import { ProgressRadial } from '@skeletonlabs/skeleton';
+	export let width: string = '';
 </script>
 
-<div class="grid place-content-center h-[500px]">
+<div class="grid place-content-center {width}">
 	<ProgressRadial
+		{width}
 		stroke={100}
 		meter="stroke-primary-500"
 		track="stroke-primary-500/30"

f25116884503df02f0da8526bda903e74acf7f60
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2024-01-23 00:01:45 -0600

    Refactor home/sources page layout

--- Sveltekit-App/src/routes/home/sources/+page.svelte
+++ Sveltekit-App/src/routes/home/sources/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Paginator from '$lib/Components/Paginator.svelte';
+	import Progress from '$lib/Components/Progress.svelte';
 	import {
 		TriggerCodeModal,
 		createOnAmountChange,
@@ -20,88 +21,101 @@
 	);
 	$: onGo = createOnGo({ ...data.params, filters, sorters }, '/home/identities');
 
-	let settings = {
-		page: Number(data.params.page),
-		limit: Number(data.params.limit),
-		size: data.totalCount,
-		amounts: [10, 50, 100, 250]
-	} satisfies PaginationSettings;
-
 	let filters = '';
 	let sorters = '';
 </script>
 
-<div class="card flex justify-center flex-col align-middle">
-	<Paginator
-		{onAmountChange}
-		{onGo}
-		{onPageChange}
-		{settings}
-		{filters}
-		{sorters}
-		totalCount={data.totalCount}
-	/>
-	<div class="table-container">
-		<table class="table">
-			<thead class="table-head">
-				<th>ID</th>
-				<th>Name</th>
-				<th>Description</th>
-				<th>Type</th>
-				<th>Authoritative</th>
-				<th>Healthy</th>
-				<th />
-			</thead>
-			<tbody class="table-body">
-				{#each data.sources as source}
-					<tr>
-						<td>
-							<p class="text-center">{source.id}</p>
-						</td>
-						<td>
-							<p class="text-center">{source.name}</p>
-						</td>
-						<td>
-							<p class="text-center">{source.description}</p>
-						</td>
-						<td>
-							<p class="text-center">{source.type}</p>
-						</td>
-						<td>
-							<p class="text-center">{source.authoritative ? 'True' : 'False'}</p>
-						</td>
-						<td>
-							<p class="text-center font-bold {source.healthy ? 'text-green-500' : 'text-red-500'}">
-								{source.healthy ? 'True' : 'False'}
-							</p>
-						</td>
-						<td class="flex flex-col justify-center gap-1">
-							<a
-								href={`/home/sources/${source.id}`}
-								class="btn variant-filled-primary text-sm text-white"
-								data-sveltekit-preload-data="hover"
-							>
-								Open
-							</a>
-							<button
-								on:click={() => TriggerCodeModal(source, modalStore)}
-								class="btn variant-filled-primary text-sm text-white"
-							>
-								View
-							</button>
-						</td>
-					</tr>
-				{/each}
-			</tbody>
-		</table>
-	</div>
-	<Paginator
-		{onAmountChange}
-		{onGo}
-		{onPageChange}
-		{settings}
-		{filters}
-		{sorters}
-		totalCount={data.totalCount}
-	/>
+<div class="card flex flex-col justify-center h-full">
+	{#await data.totalCount then totalCount}
+		<Paginator
+			{onAmountChange}
+			{onGo}
+			{onPageChange}
+			settings={{
+				page: Number(data.params.page),
+				limit: Number(data.params.limit),
+				size: totalCount,
+				amounts: [10, 50, 100, 250]
+			}}
+			{filters}
+			{sorters}
+			{totalCount}
+		/>
+	{/await}
+	{#await data.sources}
+		<Progress />
+	{:then sources}
+		<div class="table-container">
+			<table class="table">
+				<thead class="table-head">
+					<th>ID</th>
+					<th>Name</th>
+					<th>Description</th>
+					<th>Type</th>
+					<th>Authoritative</th>
+					<th>Healthy</th>
+					<th />
+				</thead>
+				<tbody class="table-body">
+					{#each sources as source}
+						<tr>
+							<td>
+								<p class="text-center">{source.id}</p>
+							</td>
+							<td>
+								<p class="text-center">{source.name}</p>
+							</td>
+							<td>
+								<p class="text-center">{source.description}</p>
+							</td>
+							<td>
+								<p class="text-center">{source.type}</p>
+							</td>
+							<td>
+								<p class="text-center">{source.authoritative ? 'True' : 'False'}</p>
+							</td>
+							<td>
+								<p
+									class="text-center font-bold {source.healthy ? 'text-green-500' : 'text-red-500'}"
+								>
+									{source.healthy ? 'True' : 'False'}
+								</p>
+							</td>
+							<td class="flex flex-col justify-center gap-1">
+								<a
+									href={`/home/sources/${source.id}`}
+									class="btn variant-filled-primary text-sm text-white"
+									data-sveltekit-preload-data="hover"
+								>
+									Open
+								</a>
+								<button
+									on:click={() => TriggerCodeModal(source, modalStore)}
+									class="btn variant-filled-primary text-sm text-white"
+								>
+									View
+								</button>
+							</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
+	{/await}
+	{#await data.totalCount then totalCount}
+		<Paginator
+			{onAmountChange}
+			{onGo}
+			{onPageChange}
+			settings={{
+				page: Number(data.params.page),
+				limit: Number(data.params.limit),
+				size: totalCount,
+				amounts: [10, 50, 100, 250]
+			}}
+			{filters}
+			{sorters}
+			{totalCount}
+		/>
+	{/await}
 </div>

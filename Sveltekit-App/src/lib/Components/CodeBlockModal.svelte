349ad4a210b1604e021180a267f55b07b928e51e
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2024-01-23 19:53:29 -0600

    Update CodeBlockModal.svelte to handle default values for language and code

--- Sveltekit-App/src/lib/Components/CodeBlockModal.svelte
+++ Sveltekit-App/src/lib/Components/CodeBlockModal.svelte
@@ -7,7 +7,7 @@
 <div class="card p-4 w-[85%]">
 	<CodeBlock
 		lineNumbers
-		language={$modalStore[0]?.meta?.language}
-		code={$modalStore[0]?.meta?.code}
+		language={$modalStore[0]?.meta?.language || 'json'}
+		code={$modalStore[0]?.meta?.code || ''}
 	/>
 </div>

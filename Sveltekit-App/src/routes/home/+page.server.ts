fee5a2b03017e3c6871bb417427d86b6d26923ee
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2024-01-24 08:05:36 -0600

    Fix import and add missing semicolons in +page.server.ts

--- Sveltekit-App/src/routes/home/+page.server.ts
+++ Sveltekit-App/src/routes/home/+page.server.ts
@@ -1,6 +1,6 @@
-import { getSession, checkSession } from "$lib/utils/oauth"
+import { getSession, checkSession } from '$lib/utils/oauth';
 export const load = async ({ cookies }) => {
-      if(!checkSession()) return { baseUrl: "", tenantUrl: ""}
-    	const session = getSession(cookies)
-      return session
+	if (!checkSession(cookies)) return { baseUrl: '', tenantUrl: '' };
+	const session = getSession(cookies);
+	return { session };
 };

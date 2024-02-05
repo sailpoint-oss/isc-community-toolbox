46ec3adf2ec88bf4deff9740770b7a3f8cf7306a
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2024-02-05 16:07:31 -0600

    Refactor server.ts to use locals object for session and idnSession

--- Sveltekit-App/src/routes/api/sailpoint/cluster/[clusterID]/+server.ts
+++ Sveltekit-App/src/routes/api/sailpoint/cluster/[clusterID]/+server.ts
@@ -4,17 +4,13 @@ import { json } from '@sveltejs/kit';
 import { ManagedClustersBetaApi } from 'sailpoint-api-client';
 
 /** @type {import('./$types').RequestHandler} */
-export async function GET({ cookies, params }) {
-	// Generic SDK setup
-	const session = JSON.parse(cookies.get('session')!);
-	const idnSession = await getToken(cookies);
-
-	const config = createConfiguration(session.baseUrl, idnSession.access_token);
-
-	// Route specific SDK call
+export async function GET({ locals, params }) {
+	
+	const config = createConfiguration(locals.session.baseUrl, locals.idnSession.access_token);
+	
 	const api = new ManagedClustersBetaApi(config);
 
 	const val = await api.getManagedCluster({ id: params.clusterID });
-	// console.log(val);
+	
 	return json(val.data);
 }
